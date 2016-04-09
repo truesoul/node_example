8c35c087502409170e2c3bb872dc2bafb1334553
truesoul <mr.summersky@gmx.de>
2016-04-09 09:33:07 +0200

    Post Example & Body Parser

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    next();
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.get('/hello', function(req, res){
     res.status(200).send("Hello World");
+});
+
+app.post('/name', function (req, res) {
+    if(req.body && req.body.name){
+        res.status(200).send("Hello "+req.body.name);
+    } else {
+        res.status(400).send("Empty body");
+    }
 });
 
 //app.use('/public', express.static(__dirname + '/public'));
